9593e3a335e97d2fca0ddbab38fed628b34b4e78
katg999 <katex911@gmail.com>
2024-05-17 21:18:59 +0300

    Test One Exercise With An Updated basics.py File

--- exercises/test_exercise_one.basics.py
+++ exercises/test_exercise_one.basics.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 def collatz(n: int) -> List[int]:
     """
     You're given a positive integer n. Write an algorithm that does the following:
@@ -12,8 +11,15 @@ def collatz(n: int) -> List[int]:
     For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
     So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
     """
-    pass
-
+    sequence = []
+    while n != 1:
+        sequence.append(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+    sequence.append(1)
+    return sequence
 
 def distinct_numbers(numbers: List[int]) -> int:
     """
@@ -21,4 +27,4 @@ def distinct_numbers(numbers: List[int]) -> int:
 
     E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
     """
-    pass
+    return len(set(numbers))
